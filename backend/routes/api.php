2536b95fecda8e76e7755537fe0c8186581b68aa
MohamadMoussa1 <moussamohamad389@gmail.com>
2025-11-29 00:30:59 +0200

    AuthController and routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    
+    // Admin-only routes
+    Route::middleware(['role:admin'])->group(function () {
+        Route::post('/admin/register', [AuthController::class, 'adminRegister']);
+    });
+});
