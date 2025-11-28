2c227e28179dec5e255a94bbcb9a2ae90345f82f
MohamadMoussa1 <moussamohamad389@gmail.com>
2025-11-29 00:30:21 +0200

    Resources

---
+++ backend/app/Http/Resources/CitizenProfileResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CitizenProfileResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'citizen',
+            'national_id' => $this->national_id,
+            'address' => $this->address,
+            'contact' => $this->contact,
+            'date_of_birth' => $this->date_of_birth->format('Y-m-d'),
+        ];
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasRoles;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
 
